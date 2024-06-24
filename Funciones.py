57389411ab2956fa976a45644d417731574f8df1
Carlos <ca.moil@duocuc.cl>
2024-06-24 12:34:21 -0400

    a

--- Funciones.py
+++ Funciones.py
@@ -55,3 +55,116 @@ def funcion_agregar(lista):
     #Guardar datos ingresados en un diccionario y agregarlos a una lista
     producto = {"codigo": codigo, "nombre": nombre, "cantidad": cantidad, "precio": precio }
     lista.append(producto)
+#Función modificar producto
+def modificar_producto(coleccion):
+    flag1 = True
+    flag2 = True
+    print("Seleccionó modificar un producto\n");
+    bandera_menu_modificacion=True;
+    print("Seleccione el producto que desea modificar ingresando el número que corresponda");
+    #For para mostrar los productos y sus respectivos códigos.
+    for i in range(len(coleccion)):
+        producto=coleccion[i];
+        print(f"Producto {i+1}: {producto}");
+    productoEncontrado={}
+    while bandera_menu_modificacion:
+        #Validamos el código ingresado por el usuario.
+        try:
+            codigo=int(input("Ingrese el código del producto el cual desee modificar: "));
+        except:
+            for i in range(len(coleccion)):
+                producto = coleccion[i]
+                if codigo==producto["nombre"]:
+                    productoEncontrado=producto
+            if productoEncontrado not in coleccion:
+                print("El producto seleccionado no existe, intentelo nuevamente");
+        else:
+            #
+            for i in range(len(coleccion)):
+                producto=coleccion[i];
+                if codigo == producto["codigo"]:
+                    print("Ya puede modificar el producto");
+                    nombre = input("Ingrese el nombre del producto: ")
+                    while flag1 == True:
+                            try:
+                                cantidad = int(input("Ingrese la cantidad de productos: "))
+                            except:
+                                print("\nLa cantidad ingresada debe ser en números. Vuelva a intentarlo...\n")
+                            else:
+                                flag1 = False
+
+                    while flag2 == True:
+                        try:
+                            precio = int(input("Ingrese el precio del producto: "))
+                        except:
+                            print("\nERROR. El precio ingresado deben ser números...\n")
+                        else:
+                            flag2 = False
+                        
+                    #Actualizar
+                    coleccion[i]={"codigo":producto["codigo"],"nombre":nombre,"cantidad":cantidad,"precio":precio}
+                    bandera_menu_modificacion=False;
+
+
+
+#Función para listar todos los productos
+def ver_productos(coleccion):
+    #Generamos la tabla que se mostrará al usuario
+    print("Seleccionó listar todos los productos\n");
+    print("------------------------------------------------------------------------");
+    print("|     CODIGO     |     NOMBRE     |     CANTIDAD     |      PRECIO     |");
+    print("------------------------------------------------------------------------");
+    #Ciclo For para mostrar todos los productos existentes.
+    for i in range(len(coleccion)):
+        producto=coleccion[i];
+        print(f"{producto["codigo"]} | {producto["nombre"]} | {producto["cantidad"]} | {producto["precio"]} |");
+
+
+
+#Guardar colección en un archivo.
+
+
+def guardar_archivo(coleccion, nombreArchivo):
+    #Coleccion formato coleccion=[{}]
+    datos="Codigo,Nombre,Cantidad,Precio\n"
+    for i in range(len(coleccion)):
+        producto=coleccion[i]
+        datos=f"{datos}{producto["codigo"]},{producto["nombre"]},{producto["cantidad"]},{producto["precio"]}"
+
+    archivoN = nombreArchivo+".txt"
+    with open(archivoN,"w") as archivo:
+        archivo.write(datos)
+
+def eliminar_producto(coleccion):
+    #Coleccion formato coleccion=[{}]
+    #Mostrar lista de datos
+    bandera_menu_eliminar=True;
+    print("Seleccione el producto que desea modificar ingresando el número que corresponda");
+
+    for i in range(len(coleccion)):
+        producto=coleccion[i];
+        print(f"Producto {i+1}: {producto}");
+    
+    
+    productoEncontrado={}
+    while bandera_menu_eliminar:
+        try:
+            codigo=int(input("Ingrese el código del producto el cual desee eliminar: "));
+            
+        except:
+            for i in range(len(coleccion)):
+                producto = coleccion[i]
+                if codigo==producto["nombre"]:
+                    productoEncontrado=producto
+            if productoEncontrado not in coleccion:
+                print("El producto seleccionado no existe, intentelo nuevamente");
+        else:
+            for i in range(len(coleccion)):
+                producto=coleccion[i]
+                if producto["codigo"]==codigo:
+                    producto[i].pop(i)
+                print(f"El producto con código {codigo} ha sido eliminado con éxito.")
+
+
+
+                    
--- main.py
+++ main.py
@@ -1,1 +1,48 @@
 #Archivo main | Integrantes de la evaluación: Mayckol Mardones, Carlos Moil, Rodrigo Vargas.
+
+
+
+#Importamos las funciones necesarias
+import time;
+
+
+
+coleccion=[]
+#Definir variables
+bandera_menu = True;
+bandera_opcion = True;
+print("¡Bienvenido a nuestra Tienda!\n");
+#Realizamos un menú para que el usuario pueda utilizar las distintas funciones que hemos creado.
+while bandera_menu:
+    print("Seleccione una de las siguientes opciones:\n");
+    print("1.Agregar un producto");
+    print("2.Ver todos los productos");
+    print("3.Modificar un producto");
+    print("4.Eliminar un producto");
+    print("5.Guardar colección en un archivo");
+    print("6.Salir del programa");
+    #Validamos que la opción ingresada por el usuario sea numérica.
+    try:
+        opcion=int(input("Elija una opción: "));
+    except:
+        print("La opción ingresada no es válida, intentelo nuevamente\n");
+    else:
+        #Utilizando la opción ingresada por el usuario, llamamos a la función correspondiente.
+        if opcion==1:
+                print();
+        elif opcion==2:
+                print("\n");
+        elif opcion==3:
+                print();
+        elif opcion==4:
+                print();
+        elif opcion==5:
+                print();
+        elif opcion==6:
+            print("Saliendo del programa...");
+            time.sleep(1);
+            bandera_menu=False;
+            bandera_opcion=False;
+        else:
+            print("La opción ingresada no está disponible, intentelo nuevamente\n");
+
